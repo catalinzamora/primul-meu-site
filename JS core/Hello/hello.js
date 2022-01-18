3b9c332c80465c42e0a8d142f1c8e1897c465173
catalinzamora <catalin.zamora@gmail.com>
2022-01-18 20:51:57 +0200

    main

---
+++ JS core/Hello/hello.js
@@ -0,0 +1,14 @@
+// ruleaza tellMe dupa 3000ms(3secunde)
+setTimeout(tellMe, 3000)
+
+// creaza o noua instructiune(functie) numita tellMe
+function tellMe() {
+    alert("Hello World!")
+    alert("Welcome to java script!")    
+}
+
+/*
+comentariu pe mai multe linii */
+
+// comentariu pentru o singura linie
+
