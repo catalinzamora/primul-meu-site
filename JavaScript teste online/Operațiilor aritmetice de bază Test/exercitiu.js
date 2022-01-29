c53b8740ddad6b6b79b190501e1b07f17494204b
catalinzamora <catalin.zamora@gmail.com>
2022-01-29 16:07:28 +0200

    main

--- JavaScript teste online/Operațiilor aritmetice de bază Test/exercitiu.js
+++ JavaScript teste online/Operațiilor aritmetice de bază Test/exercitiu.js
@@ -1,34 +1,45 @@
 var a = prompt("Put the first numar!")
 var b =prompt("What you want to do, + , - , * , / ")
 var c =prompt("Put the second number!")
+var sum = parseInt(a) + parseInt(c)
+var divide = parseInt(a) / parseInt(c)
+var diference = parseInt(a) - parseInt(c)
+var multiply= parseInt(a) * parseInt(c)
 
+switch (sum) {
+    case sum:
+        console.log(sum)
+        break;
 
-var sum = a + c
-var diference = a - c
-var multiply = a * c
-var divide = a / c
+    case multiply:
+        console.log(multiply)
 
+    default:
+        break;
+}
 
 /*
-if (sum) {
-    console.log(a + c)
+if (Number) {
     alert("Sum is:" + sum)
+    console.log(sum)}
     
-}
-else if (diference) {
-    console.log(a - c)
-    alert("Diference is:" + diference)
-     
-} else if (multiply){
-    console.log(a * c)
 
-}
-else if (divide){
-    console.log(a / c)
-}
-else{
-    console.log("NaN")
-}*/
+else if(Number) {
+    alert("Diference is:" + diference)
+    console.log(diference)}
+
+     
+ else if (Number){
+    alert("Multiply is:" + multiply)
+    console.log(multiply)}
+
+
+else if (Number){
+    alert("Divide is:" + divide)
+    console.log(divide)} */
+
+
+
 
 
 
