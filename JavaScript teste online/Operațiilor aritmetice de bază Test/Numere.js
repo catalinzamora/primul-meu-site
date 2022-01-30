9f7eb5554680503b5223dd281fd62d35ff06e271
catalinzamora <catalin.zamora@gmail.com>
2022-01-30 10:16:35 +0200

    main

---
+++ JavaScript teste online/Operațiilor aritmetice de bază Test/Numere.js
@@ -0,0 +1,42 @@
+var a = prompt("Put the first numar!")
+var b =prompt("What you want to do, + , - , * , / ")
+var c =prompt("Put the second number!")
+
+
+
+var sum = parseInt(a) + parseInt(c)
+var diference = parseInt(a) - parseInt(c)
+var multiply = parseInt(a) * parseInt(c)
+var divide = parseInt(a) / parseInt(c)
+
+if (sum) {
+    alert("Suma este:" + sum)
+    console.log(sum)
+}
+
+ if(diference) {
+    alert("Diference is:" + diference)
+    console.log(diference)}
+
+     
+ else if (multiply){
+    alert("Multiply is:" + multiply)
+    console.log(multiply)}
+
+
+else if (divide){
+    alert("Divide is:" + divide)
+    console.log(divide)} 
+
+
+
+
+
+   
+
+
+
+
+
+
+
